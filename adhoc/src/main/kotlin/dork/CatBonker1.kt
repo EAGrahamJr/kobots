555c26f8375b5a028ad3c514a7859b1445075520
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-01-09 11:47:48 -0800

    Just tweaking things :grin:

--- adhoc/src/main/kotlin/dork/CatBonker1.kt
+++ adhoc/src/main/kotlin/dork/CatBonker1.kt
@@ -53,7 +53,7 @@ fun main() {
         while (running.get()) {
             LocalTime.now().also { time ->
                 if (time.hour >= 23 || time.hour < 8) {
-                    // first thing, turn it off and send the pointer "home"
+                    // nighttime: turn it off and send the pointer "home"
                     if (lcd.isBacklightEnabled) {
                         lcd.isBacklightEnabled = false
                         servo.value = 0f
@@ -62,15 +62,18 @@ fun main() {
                     sleepSeconds(300)
                 } else {
                     // SHOWTIME!
-                    if (!lcd.isBacklightEnabled) lcd.isBacklightEnabled = true
+                    with(lcd) {
+                        if (!isBacklightEnabled) isBacklightEnabled = true
+                        timeAndTemp()
+                    }
 
-                    lcd.timeAndTemp()
                     rangeFinder.distanceCm.run {
                         servo.distance(this)
                         lcd.distance(this)
                     }
 
-                    led.value = if (irSensor.value) 1f else 0f
+                    led.heat(irSensor.value)
+
                     busySleep(1_000_000) // 1 ms
                 }
             }
@@ -78,6 +81,11 @@ fun main() {
     }
 }
 
+// inputs
+val rangeFinder by lazy { HCSR04(23, 24) }
+val irSensor by lazy { DigitalInputDevice(21) }
+
+// outputs
 val LCD by lazy {
     HD44780_Lcd.Pi4Line.apply {
         cursorOff()
@@ -94,18 +102,10 @@ val LCD by lazy {
         createChar(1, LcdInterface.Characters.get("smilie"))
         createChar(2, LcdInterface.Characters.get("space_invader"))
         createChar(3, LcdInterface.Characters.get("heart"))
-        displayText(KOBOT_NAME)
     }
 }
 
 fun HD44780Lcd.timeAndTemp() {
-//    val hour = LocalTime.now().hour
-//    if (isBacklightEnabled) {
-//        if (hour >= 23 || hour < 8) isBacklightEnabled = false
-//    } else {
-//        if (hour >= 8 && hour < 23) isBacklightEnabled = true
-//    }
-
     val temp = (Files.readAllLines(Paths.get("/sys/class/thermal/thermal_zone0/temp")).first().toFloat() / 1000)
     val time = LocalTime.now().toString().substringBefore(".")
 
@@ -113,29 +113,22 @@ fun HD44780Lcd.timeAndTemp() {
     addText(time)
 
     setText(2, "Temp: ${temp.stringify(2)}     ")
-    addText(if (temp > 48f) 0 else 1)
+    addText(if (temp > 56f) 0 else 1)
 }
 
-var lastAngle = -1f
 fun HD44780Lcd.distance(dist: Float) {
     setText(3, "Dist: ${dist.stringify()} cm  ")
     addText(if (dist < 10) 2.toChar() else ' ')
 }
 
-val rangeFinder by lazy { HCSR04(23, 24) }
-
-val irSensor by lazy { DigitalInputDevice(21) }
-
-val servo: ServoDevice by lazy { ServoDevice.Builder(17).setTrim(ServoTrim.TOWERPRO_SG90).build() }
-
 // basically flips up the arm of the CatBonker if something gets too close
+val servo: ServoDevice by lazy { ServoDevice.Builder(17).setTrim(ServoTrim.TOWERPRO_SG90).build() }
 fun ServoDevice.distance(dist: Float) {
-    (if (dist < 20f) 90f else 0f).apply {
-        if (this != lastAngle) {
-            lastAngle = this
-            angle = lastAngle
-        }
-    }
+    angle = (if (dist < 20f) 90f else 0f)
 }
 
+// LED on/off based on the IR sensor
 val led by lazy { PwmLed(27) }
+fun PwmLed.heat(sensorValue: Boolean) {
+    value = if (sensorValue) 1f else 0f
+}
