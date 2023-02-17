c1af4c18eac663bcfe46274cc4da4d8e534733a7
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-02-17 11:49:36 -0800

    Verified new CRICKIT is working on previously dead ports

--- adhoc/src/main/kotlin/kobots/ops/Schwing.kt
+++ adhoc/src/main/kotlin/kobots/ops/Schwing.kt
@@ -19,6 +19,7 @@ package kobots.ops
 import com.diozero.api.ServoTrim
 import crackers.kobots.devices.at
 import crackers.kobots.devices.expander.CRICKITHatDeviceFactory
+import crackers.kobots.devices.set
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.runBlocking
@@ -30,7 +31,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 val zFlow = MutableSharedFlow<Boolean>(extraBufferCapacity = 5, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 val xFlow = MutableSharedFlow<Float>(extraBufferCapacity = 5, onBufferOverflow = BufferOverflow.DROP_OLDEST)
-
+val yFlow = MutableSharedFlow<Float>(extraBufferCapacity = 5, onBufferOverflow = BufferOverflow.DROP_OLDEST)
 private val running = AtomicBoolean(true)
 
 /**
@@ -40,26 +41,34 @@ fun main() {
     System.setProperty("diozero.remote.hostname", "marvin.local")
 
     CRICKITHatDeviceFactory().use {
-        val servo = it.servo(4, ServoTrim.TOWERPRO_SG90)
+        val servo = it.servo(2, ServoTrim.TOWERPRO_SG90)
         val xAxis = it.signalAnalogIn(6)
         val yAxis = it.signalAnalogIn(7)
         val zButton = it.signalDigitalIn(8)
+        val noodle = it.signalDigitalOut(1)
 
-        registerConsumer(zFlow) { value ->
-            if (value) running.set(false)
+        registerConsumer(zFlow) {
+            if (it) running.set(false)
         }
-        registerConsumer(xFlow) { value ->
-            servo at 180 * value
+        registerConsumer(xFlow) {
+            servo at 180 * it
         }
         registerConsumer(theFlow) {
             if (it is Double) println("Temp $it")
         }
+        registerConsumer(yFlow) {
+            noodle set (it > .5f)
+        }
 
         registerPublisher(xFlow) { xAxis.unscaledValue }
+        registerPublisher(yFlow) { yAxis.unscaledValue }
         registerPublisher(zFlow) { zButton.value }
         registerPublisher(theFlow, Duration.ofSeconds(1)) { cpuTemp() }
 
-        runBlocking { while (running.get()) yield() }
+        runBlocking {
+            while (running.get()) yield()
+            noodle set false
+        }
     }
 }
 
