88455f440c50882e01be91fcb874355cae4738be
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-04-29 21:01:42 -0700

    Error handlng and some more comments.

--- button-board/src/main/kotlin/crackers/kobots/buttonboard/App.kt
+++ button-board/src/main/kotlin/crackers/kobots/buttonboard/App.kt
@@ -20,26 +20,23 @@ import com.diozero.util.SleepUtil
 import crackers.kobots.devices.lighting.NeoKey
 import crackers.kobots.utilities.GOLDENROD
 import crackers.kobots.utilities.PURPLE
+import org.slf4j.LoggerFactory
 import java.awt.Color
 import java.time.Duration
+import java.time.LocalTime
 
-// TODO temporary while testing
-const val REMOTE_PI = "diozero.remote.hostname"
-const val USELESS = "useless.local"
-
-val keyboard by lazy {
+private val keyboard by lazy {
     NeoKey().apply { pixels.brightness = 0.05f }
 }
 
 // just to keep from spinning in a stupid tight loop
-val ACTIVE_DELAY = Duration.ofMillis(50).toNanos()
-val SLEEP_DELAY = Duration.ofSeconds(1).toNanos()
+private val ACTIVE_DELAY = Duration.ofMillis(50).toNanos()
+private val SLEEP_DELAY = Duration.ofSeconds(1).toNanos()
 
 /**
  * Uses NeoKey 1x4 as a HomeAssistant controller (and likely other things).
  */
 fun main() {
-//    System.setProperty(REMOTE_PI, USELESS)
     Screen.startupSequence()
 
     var running = true
@@ -50,56 +47,61 @@ fun main() {
     var lastCurrent: List<String>? = null
 
     while (running) {
-        /*
-         * This is purely button driven, so use the buttons - try to "debounce" by only detecting changes between
-         * iterations. This is because humans are slow
-         */
-        val currentButtons = keyboard.read()
-        var whichButtonsPressed: List<Int> = emptyList()
+        try {
+            brightness() // adjust per time of day
+            /*
+             * This is purely button driven, so use the buttons - try to "debounce" by only detecting changes between
+             * iterations. This is because humans are slow
+             */
+            val currentButtons = keyboard.read()
+            var whichButtonsPressed: List<Int> = emptyList()
 
-        // no change, don't anything, otherwise figure out what was asked for
-        if (currentButtons == lastButtonsRead) {
-            if (!Screen.on) buttonsWait()
-        } else {
-            lastButtonsRead = currentButtons
-            whichButtonsPressed = currentButtons.mapIndexedNotNull { index, b ->
-                if (b) {
-                    keyboard[index] = Color.YELLOW
-                    index
-                } else {
-                    null
-                }
-            }
-
-            // only do anything if the screen is on
-            currentMenu = if (Screen.on) {
-                Menu.execute(whichButtonsPressed).mapIndexed { index, item ->
-                    keyboard[index] = when (item.type) {
-                        Menu.ItemType.NOOP -> Color.BLACK
-                        Menu.ItemType.ACTION -> Color.GREEN
-                        Menu.ItemType.NEXT -> Color.CYAN
-                        Menu.ItemType.PREV -> Color.BLUE
-                        Menu.ItemType.EXIT -> Color.RED
-                    }
-                    item.name
-                }
+            // no change, don't anything, otherwise figure out what was asked for
+            if (currentButtons == lastButtonsRead) {
+                if (!Screen.on) buttonsWait()
             } else {
-                buttonsWait()
-                emptyList()
+                lastButtonsRead = currentButtons
+                whichButtonsPressed = currentButtons.mapIndexedNotNull { index, b ->
+                    if (b) {
+                        keyboard[index] = Color.YELLOW
+                        index
+                    } else {
+                        null
+                    }
+                }
+
+                // only do anything if the screen is on
+                currentMenu = if (Screen.on) {
+                    Menu.execute(whichButtonsPressed).mapIndexed { index, item ->
+                        keyboard[index] = when (item.type) {
+                            Menu.ItemType.NOOP -> Color.BLACK
+                            Menu.ItemType.ACTION -> Color.GREEN
+                            Menu.ItemType.NEXT -> Color.CYAN
+                            Menu.ItemType.PREV -> Color.BLUE
+                            Menu.ItemType.EXIT -> Color.RED
+                        }
+                        item.name
+                    }
+                } else {
+                    buttonsWait()
+                    emptyList()
+                }
             }
-        }
 
-        if (currentMenu != lastCurrent) {
-            lastCurrent = currentMenu
-        }
+            if (currentMenu != lastCurrent) {
+                lastCurrent = currentMenu
+            }
 
-        if (currentMenu.isEmpty() && whichButtonsPressed.contains(3)) {
-            running = false
-        } else {
-            val buttonWasPressed = whichButtonsPressed.isNotEmpty()
-            Screen.execute(buttonWasPressed, currentMenu)
+            if (currentMenu.isEmpty() && whichButtonsPressed.contains(3)) {
+                running = false
+            } else {
+                val buttonWasPressed = whichButtonsPressed.isNotEmpty()
+                Screen.execute(buttonWasPressed, currentMenu)
+            }
+            SleepUtil.busySleep(if (Screen.on) ACTIVE_DELAY else SLEEP_DELAY)
+        } catch (e: Throwable) {
+            LoggerFactory.getLogger("ButtonBox").error("Error found - continuing", e)
         }
-        SleepUtil.busySleep(if (Screen.on) ACTIVE_DELAY else SLEEP_DELAY)
     }
     keyboard[3] = GOLDENROD
 
@@ -107,9 +109,24 @@ fun main() {
     keyboard.close()
 }
 
+/**
+ * Set "sleeping" colors on the buttons.
+ */
 private fun buttonsWait() {
     if ((keyboard color 0).color != PURPLE) {
         (0..2).forEach { keyboard[it] = PURPLE }
         keyboard[3] = Color.RED
+    }
+}
+
+/**
+ * Adjust brightness according to the hour of day.
+ */
+private fun brightness() {
+    LocalTime.now().also { t ->
+        if (t.minute == 0) {
+            val b = if (t.hour >= 22 || t.hour < 8) .01f else .05f
+            if (b != keyboard.pixels.brightness) keyboard.pixels.brightness = b
+        }
     }
 }
