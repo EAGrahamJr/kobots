cc2764aae2f84950c32d73f6e119d65f10fb217c
E. A. Graham Jr <10370165+EAGrahamJr@users.noreply.github.com>
2023-05-02 09:57:07 -0700

    Don't need this right now

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20-RC")
     implementation("com.github.johnrengelman:shadow:8.1.1")
     implementation("org.jmailen.gradle:kotlinter-gradle:3.12.0")
-    implementation("crackers.buildstuff:crackers-gradle-plugins:1.0.0")
+    // TODO uncomment if there's something to publish
+//    implementation("crackers.buildstuff:crackers-gradle-plugins:1.0.0")
 }
